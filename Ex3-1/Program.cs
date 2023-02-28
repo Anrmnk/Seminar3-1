5203b093146340f47d75819320f2d4e5676cc717
Anrmnk <anrmnk@gmail.com>
2023-02-28 22:42:04 +0300

    1 task

---
+++ Ex3-1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Введите первое число");
+string user1 = Console.ReadLine ();
+int number1 = Convert.ToInt32(user1);
+Console.Write ("Введите второе число");
+string user2 = Console.ReadLine ();
+int number2 = Convert.ToInt32(user2);
+if (number1 == number2 * number2)
+{
+    Console.WriteLine ("Да");
+}
+else
+{
+    Console.WriteLine ("Нет");
+}

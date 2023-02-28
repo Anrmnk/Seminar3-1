e978b03824cb43efc589baa597470b4ecf8aa1a1
Anrmnk <anrmnk@gmail.com>
2023-03-01 00:14:26 +0300

    5 task

---
+++ Ex3-5/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите число");
+string usernumber = Console.ReadLine ();
+int number = Convert.ToInt32(usernumber);
+int max = number;
+int min = -number;
+while (min <= max)
+{
+   Console.Write (min + " ");
+    min = min + 1;
+}
